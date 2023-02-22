a6935fc80ce8ddca36c4e04a853dc186964e1b27
Marcin Morawski <marcin@morawskim.pl>
2023-02-22 17:50:46 +0100

    [noip-client] Add slog

--- noip-client/cmd/update.go
+++ noip-client/cmd/update.go
@@ -1,9 +1,9 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/go-playground/validator/v10"
 	"github.com/spf13/cobra"
+	"golang.org/x/exp/slog"
 	"noip-client/internal/config"
 	"noip-client/internal/iphelper"
 	"noip-client/internal/noip"
@@ -18,25 +18,27 @@ var versionCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Update the current IP address assigned to noip domain",
 	Run: func(cmd *cobra.Command, args []string) {
+		logger := slog.New(slog.NewJSONHandler(os.Stdout))
+		logger.Enabled(slog.LevelInfo)
 		noIpConfig := config.CreateNoIpConfigFromEnvVariables()
 
 		v := validator.New()
 		err := v.Struct(noIpConfig)
 		if err != nil {
-			fmt.Println(err)
+			logger.Error("Missing or invalid evn variables:", err)
 			os.Exit(1)
 		}
 
 		ipHelper := iphelper.NewIpHelper()
 		currentAssignedIp, err := ipHelper.GetCurrentAssignedIp(noIpConfig.Hostname)
 		if err != nil {
-			fmt.Println(err)
+			logger.Error("Unable to get current assigned IP address", err, slog.String("hostname", noIpConfig.Hostname))
 			os.Exit(1)
 		}
 
 		myCurrentPublicIp, err := ipHelper.GetCurrentPublicIpAddress()
 		if err != nil {
-			fmt.Println(err)
+			logger.Error("Unable to get current public IP address", err)
 			os.Exit(1)
 		}
 
@@ -44,9 +46,11 @@ var versionCmd = &cobra.Command{
 			noipApiClient := noip.NewApiClient(noIpConfig)
 			updateApiErr := noipApiClient.UpdateAssignedIp(currentAssignedIp)
 			if updateApiErr != nil {
-				fmt.Println(updateApiErr)
+				logger.Error("Unable to update assigned noip address", updateApiErr, slog.String("hostname", noIpConfig.Hostname), slog.String("username", noIpConfig.Username))
 				os.Exit(1)
 			}
+		} else {
+			logger.Info("Current assigned IP address is the same as public address", slog.String("noip", currentAssignedIp.String()), slog.String("publicIp", myCurrentPublicIp.String()))
 		}
 	},
 }
--- noip-client/go.mod
+++ noip-client/go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/exp v0.0.0-20230213192124-5e25df0256eb // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
