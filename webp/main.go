f59e759ed19ccebdbc35846f5fe9b43deda3750e
Marcin Morawski <marcin@morawskim.pl>
2023-03-16 15:52:44 +0100

    [webo] Refactoring

--- webp/main.go
+++ webp/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/chai2010/webp"
@@ -18,7 +19,7 @@ import (
 func fileExists(filename string) bool {
 	info, err := os.Stat(filename)
 
-	if os.IsNotExist(err) {
+	if errors.Is(err, os.ErrNotExist) {
 		return false
 	}
 
@@ -61,75 +62,112 @@ func isSupportedMimeType(mimeType string) (bool, string) {
 	return false, ""
 }
 
+func getAbsolutePathToFile(filePathFromArgs *string) (string, error) {
+	absFilePath, err := filepath.Abs(*filePathFromArgs)
+	if err != nil {
+		return "", fmt.Errorf("cannot parse path %v", *filePathFromArgs)
+	}
+
+	if !fileExists(absFilePath) {
+		return "", fmt.Errorf("file %v not exists\n", absFilePath)
+	}
+
+	return absFilePath, nil
+}
+
+func convertImgToWebp(img image.Image) (*bytes.Buffer, error) {
+	var webpBuf bytes.Buffer
+
+	if err := webp.Encode(&webpBuf, img, &webp.Options{}); err != nil {
+		return nil, fmt.Errorf("cannot encode image to webp format - %v", err)
+	}
+
+	return &webpBuf, nil
+}
+
+func createWebpThumbnail(img image.Image) (*bytes.Buffer, error) {
+	var webpThumbnailBuf bytes.Buffer
+	thumbnail := imaging.Resize(img, 100, 0, imaging.Lanczos)
+
+	if err := webp.Encode(&webpThumbnailBuf, thumbnail, &webp.Options{}); err != nil {
+		return nil, fmt.Errorf("cannot create thumbnail - %v", err)
+	}
+
+	return &webpThumbnailBuf, nil
+}
+
+func saveWebpImg(path string, buffer *bytes.Buffer) error {
+	if err := os.WriteFile(path, buffer.Bytes(), 0644); err != nil {
+		return fmt.Errorf("cannot save webp - %v", err)
+	}
+
+	return nil
+}
+
 func main() {
-	var webpBuf, webpThumbnailBuf bytes.Buffer
 	var err error
 
 	filePathFromArgs := flag.String("image", "", "path to image")
 	flag.Parse()
 
-	absFilePath, err := filepath.Abs(*filePathFromArgs)
+	filePath, err := getAbsolutePathToFile(filePathFromArgs)
 	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "Cannot parse path %v", *filePathFromArgs)
+		_, _ = fmt.Fprintf(os.Stderr, "Cannot get abosulte path for file %v: %v", *filePathFromArgs, err)
 		os.Exit(1)
 	}
 
-	filePath := &absFilePath
-
-	if !fileExists(*filePath) {
-		_, _ = fmt.Fprintf(os.Stderr, "File %v not exists\n", *filePath)
-		os.Exit(1)
-	}
-
-	mtype, err := mimetype.DetectFile(*filePath)
+	mtype, err := mimetype.DetectFile(filePath)
 	ok, imgFormat := isSupportedMimeType(mtype.String())
 	if !ok {
-		_, _ = fmt.Fprintf(os.Stderr, "File %v does not look like image (%v)\n", *filePath, mtype.String())
+		_, _ = fmt.Fprintf(os.Stderr, "File %v does not look like image (%v)\n", filePath, mtype.String())
 		os.Exit(1)
 	}
 
-	imgFileBuf, err := os.ReadFile(*filePath)
+	imgFileBuf, err := os.ReadFile(filePath)
 	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "File %v cannot be read - %v\n", *filePath, err)
+		_, _ = fmt.Fprintf(os.Stderr, "File %v cannot be read - %v\n", filePath, err)
 		os.Exit(1)
 	}
 
 	img, err := convertBufToImage(imgFileBuf, imgFormat)
-
 	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "Cannot parse image %v for reason %v\n", *filePath, err)
+		_, _ = fmt.Fprintf(os.Stderr, "Cannot parse image %v for reason %v\n", filePath, err)
 		os.Exit(1)
 	}
 
 	// convert to webp and create miniature
-	thumbnail := imaging.Resize(img, 100, 0, imaging.Lanczos)
-
-	if err = webp.Encode(&webpBuf, img, &webp.Options{}); err != nil {
+	webpBuf, err := convertImgToWebp(img)
+	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Cannot encode image to webp format - %v", err)
 		os.Exit(1)
 	}
 
-	if err = webp.Encode(&webpThumbnailBuf, thumbnail, &webp.Options{}); err != nil {
+	webpThumbnailBuf, err := createWebpThumbnail(img)
+	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Cannot create thumbnail - %v", err)
 		os.Exit(1)
 	}
 
-	webpOutputPath := filepath.Join(
-		filepath.Dir(absFilePath),
-		strings.TrimSuffix(filepath.Base(absFilePath), filepath.Ext(absFilePath))+".webp",
+	err = saveWebpImg(
+		filepath.Join(
+			filepath.Dir(filePath),
+			strings.TrimSuffix(filepath.Base(filePath), filepath.Ext(filePath))+".webp",
+		),
+		webpBuf,
 	)
-
-	webpThumbnailOutputPath := filepath.Join(
-		filepath.Dir(absFilePath),
-		strings.TrimSuffix(filepath.Base(absFilePath), filepath.Ext(absFilePath))+"_thumbnail.webp",
-	)
-
-	if err = os.WriteFile(webpOutputPath, webpBuf.Bytes(), 0644); err != nil {
+	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Cannot save webp - %v", err)
 		os.Exit(1)
 	}
 
-	if err = os.WriteFile(webpThumbnailOutputPath, webpThumbnailBuf.Bytes(), 0644); err != nil {
+	err = saveWebpImg(
+		filepath.Join(
+			filepath.Dir(filePath),
+			strings.TrimSuffix(filepath.Base(filePath), filepath.Ext(filePath))+"_thumbnail.webp",
+		),
+		webpThumbnailBuf,
+	)
+	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Cannot save thumbnail - %v", err)
 		os.Exit(1)
 	}
