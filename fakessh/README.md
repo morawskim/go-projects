9294fa785bd4389262c2d2db5bd52c983c60da87
Marcin Morawski <marcin@morawskim.pl>
2023-03-18 13:05:41 +0100

    Init fakessh project

---
+++ fakessh/README.md
@@ -0,0 +1,7 @@
+
+For better testing experience add this to your `~/.ssh/config file`
+
+```
+Host localhost
+    UserKnownHostsFile /dev/null
+```
---
+++ fakessh/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gliderlabs/ssh"
+	"golang.org/x/exp/slog"
+	"net"
+	"os"
+	"time"
+)
+
+type contextKey string
+
+func (c contextKey) String() string {
+	return "_fake_ssh_ctx_" + string(c)
+}
+
+var (
+	contextKeySlog = contextKey("slog")
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout))
+	logger.Enabled(nil, slog.LevelInfo)
+
+	// Create a new server instance
+	s := &ssh.Server{
+		Addr: "127.0.0.1:2222",
+		// handler will never been called, because we deny all authentication request
+		Handler:         handleSession,
+		PasswordHandler: handleAuthentication,
+		IdleTimeout:     60 * time.Second,
+		ConnCallback: func(ctx ssh.Context, conn net.Conn) net.Conn {
+			ctx.SetValue(contextKeySlog, logger)
+			return conn
+		},
+	}
+
+	logger.Info(fmt.Sprintf("Starting SSH server on address: %v", s.Addr))
+	err := s.ListenAndServe()
+
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+}
+
+func handleSession(s ssh.Session) {
+	// this code will never execute because neither user is allowed to connect
+	s.Write([]byte("Hello World!"))
+	s.Close()
+}
+
+func handleAuthentication(ctx ssh.Context, passwd string) bool {
+	logger := ctx.Value(contextKeySlog).(*slog.Logger)
+
+	logger.Info(
+		"New authentication request",
+		slog.String("username", ctx.User()),
+		slog.String("password", passwd),
+		slog.String("remote", ctx.RemoteAddr().String()),
+	)
+
+	// deny all connections
+	return false
+}
