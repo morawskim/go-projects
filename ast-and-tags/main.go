11f07854a67289ce1ca260b669b1a031a389bab5
Marcin Morawski <marcin@morawskim.pl>
2023-05-13 13:34:11 +0200

    [ast-and-tags] Parse godocs

--- ast-and-tags/main.go
+++ ast-and-tags/main.go
@@ -2,19 +2,44 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
+	goparser "go/parser"
+	"go/token"
 	"reflect"
 	"strconv"
 )
 
+// @MyAnnotation This text is description of User struct
 type User struct {
 	fullName string `mytag:"foo"`
 	age      int    `myIntTag:"123"`
 }
 
+// @MyAnnotation This text is description of main function
 func main() {
 	t := reflect.TypeOf(User{})
 	displayMyTagValue(t)
 	displayMyIntTagValue(t)
+
+	fset := token.NewFileSet()
+	astFile, err := goparser.ParseFile(fset, "main.go", nil, goparser.ParseComments)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, v := range astFile.Scope.Objects {
+		if v.Name == "main" {
+			funcDec, ok := v.Decl.(*ast.FuncDecl)
+			if !ok {
+				panic("The Decl is not FuncDecl")
+			}
+
+			for _, c := range funcDec.Doc.List {
+				fmt.Printf("%v\n", c.Text)
+			}
+		}
+	}
 }
 
 func displayMyTagValue(t reflect.Type) {
