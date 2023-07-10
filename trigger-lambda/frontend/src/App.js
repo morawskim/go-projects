87e8a94eb15153ad90d7edb131d93318c9b883f8
Marcin Morawski <marcin@morawskim.pl>
2023-07-10 19:18:12 +0200

    [trigger-aws-lambda] Migrate to react-select

--- trigger-lambda/frontend/src/App.js
+++ trigger-lambda/frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import validator from '@rjsf/validator-ajv8';
 import Form from '@rjsf/core';
+import Select from 'react-select'
 
 const schema = {
     title: 'S3/PutObject',
@@ -18,8 +19,18 @@ const JsonSchemasForEvents = {
 }
 
 const TEMPLATES = {
-    's3/put-object': `{"Records":[{"eventVersion":"2.0","eventSource":"aws:s3","awsRegion":"eu-central-1","eventTime":"1970-01-01T00:00:00.000Z","eventName":"ObjectCreated:Put","userIdentity":{"principalId":"EXAMPLE"},"requestParameters":{"sourceIPAddress":"127.0.0.1"},"responseElements":{"x-amz-request-id":"EXAMPLE123456789","x-amz-id-2":"EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH"},"s3":{"s3SchemaVersion":"1.0","configurationId":"testConfigRule","bucket":{"name":"<<BUCKET_NAME>>","ownerIdentity":{"principalId":"EXAMPLE"},"arn":"arn:aws:s3:::data.linker.shop"},"object":{"key":"<<OBJECT_KEY>>","size":1024,"eTag":"0123456789abcdef0123456789abcdef","sequencer":"0A1B2C3D4E5F678901"}}}]}`,
+    's3/put-object': `{"Records":[{"eventVersion":"2.0","eventSource":"aws:s3","awsRegion":"eu-central-1","eventTime":"1970-01-01T00:00:00.000Z","eventName":"ObjectCreated:Put","userIdentity":{"principalId":"EXAMPLE"},"requestParameters":{"sourceIPAddress":"127.0.0.1"},"responseElements":{"x-amz-request-id":"EXAMPLE123456789","x-amz-id-2":"EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH"},"s3":{"s3SchemaVersion":"1.0","configurationId":"testConfigRule","bucket":{"name":"<<BUCKET_NAME>>","ownerIdentity":{"principalId":"EXAMPLE"},"arn":"arn:aws:s3:::<<BUCKET_NAME>>"},"object":{"key":"<<OBJECT_KEY>>","size":1024,"eTag":"0123456789abcdef0123456789abcdef","sequencer":"0A1B2C3D4E5F678901"}}}]}`,
 }
+
+const OPTIONS = [
+    {value: '', label: 'Select event'},
+    {
+        label: 'S3',
+        options: [
+            {value: 's3/put-object', label: 'S3/PutObject'}
+        ],
+    },
+]
 
 function App() {
     const [formData, setFormData] = React.useState({});
@@ -33,16 +44,14 @@ function App() {
 
                     <div className="mb-3">
                         <label htmlFor="event" className="form-label">Email address</label>
-                        <select defaultValue="" id="event" className="form-select" aria-label="Default select example"
-                                onChange={event => {
-                                    setEventName(event.target.value);
-                                    setFormData({});
-                                }}>
-                            <option value="">Select event</option>
-                            <optgroup label="S3">
-                                <option value="s3/put-object">PutObject</option>
-                            </optgroup>
-                        </select>
+                        <Select
+                            defaultValue={OPTIONS[0]}
+                            id={"event"}
+                            onChange={(event) => {
+                                setEventName(event.value);
+                                setFormData({});
+                            }}
+                            options={OPTIONS} />
                     </div>
 
                     <Form
@@ -62,7 +71,6 @@ function App() {
                         event.preventDefault();
                         event.stopPropagation();
 
-                        const eventName = document.getElementById('event').value;
                         if (!TEMPLATES.hasOwnProperty(eventName)) {
                             return;
                         }
