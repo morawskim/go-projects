ce6743f481fb7831d4ba403be9104143fd694140
Marcin Morawski <marcin@morawskim.pl>
2023-08-07 16:53:11 +0200

    [protobuf & gRPC] generate random protobuf message

--- protobuf/php/index.php
+++ protobuf/php/index.php
@@ -2,16 +2,30 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$faker = Faker\Factory::create('en');
+
 $timestamp = new \Google\Protobuf\Timestamp();
-$timestamp->fromDateTime(new DateTime());
+$timestamp->fromDateTime($faker->dateTimeBetween('-7 days'));
+
+$numbOfItems = $faker->numberBetween(1, 4);
+$items = [];
 
 $order = new \PB\Order();
-$order->setNumber("LI/PHP/123456789");
-$order->setStatus(\PB\OrderStatus::ORDER_STATUS_NEW);
-$order->setItems([
-    (new \PB\OrderItem())->setSku("456789")->setQuantity(2)->setUnitPrice(499),
-    (new \PB\OrderItem())->setSku("AZ456")->setQuantity(1)->setUnitPrice(999),
-]);
+$order->setNumber(sprintf("LI/PHP/%s", $faker->randomNumber(6)));
+$order->setStatus($faker->randomElement([
+    \PB\OrderStatus::ORDER_STATUS_NEW,
+    \PB\OrderStatus::ORDER_STATUS_ACCEPTED,
+    \PB\OrderStatus::ORDER_STATUS_CANCALED,
+    \PB\OrderStatus::ORDER_STATUS_PAID,
+    \PB\OrderStatus::ORDER_STATUS_SENT,
+]));
+for ($i = 0; $i < $numbOfItems; $i++) {
+    $items[] = (new \PB\OrderItem())
+        ->setSku($faker->ean8())
+        ->setQuantity($faker->numberBetween(1, 5))
+        ->setUnitPrice($faker->numberBetween(199, 499));
+}
+$order->setItems($items);
 $order->setCreatedAt($timestamp);
 
 $binaryString = $order->serializeToString();
