65d98ed08fd33c3e10d05dcf668ce1e21cc4fe01
Marcin Morawski <marcin@morawskim.pl>
2023-10-26 16:47:16 +0200

    [dashboard] Http server

--- dashboard/main.go
+++ dashboard/main.go
@@ -1,34 +1,73 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
-	"fmt"
 	ics "github.com/arran4/golang-ical"
 	"net/http"
+	"time"
 )
+
+type event struct {
+	Summary     string
+	Description string
+	StartAt     *time.Time
+	EndAt       *time.Time
+}
 
 func main() {
 	var url = flag.String("url", "", "The URL to iCal")
 	flag.Parse()
 
-	get, err := http.Get(*url)
-	if err != nil {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/calendar", func(w http.ResponseWriter, r *http.Request) {
+		get, err := http.Get(*url)
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+
+			return
+		}
+
+		defer get.Body.Close()
+		cal, err := ics.ParseCalendar(get.Body)
+
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
+
+			return
+		}
+
+		events := make([]event, len(cal.Events()), len(cal.Events()))
+
+		for i, val := range cal.Events() {
+			var startAt, endAt *time.Time
+
+			tmp, err := val.GetStartAt()
+			startAt = timeOrNull(tmp, err)
+			tmp, err = val.GetEndAt()
+			endAt = timeOrNull(tmp, err)
+
+			events[i] = event{
+				Summary:     val.GetProperty(ics.ComponentPropertySummary).Value,
+				Description: val.GetProperty(ics.ComponentPropertyDescription).Value,
+				StartAt:     startAt,
+				EndAt:       endAt,
+			}
+		}
+		json.NewEncoder(w).Encode(events)
+	})
+
+	if err := http.ListenAndServe(":8000", mux); err != nil {
 		panic(err)
 	}
+}
 
-	defer get.Body.Close()
-
-	cal, err := ics.ParseCalendar(get.Body)
-
-	if err != nil {
-		panic(err)
-	}
-
-	for _, val := range cal.Events() {
-		fmt.Println(val.GetProperty(ics.ComponentPropertySummary).Value)
-		fmt.Println(val.GetProperty(ics.ComponentPropertyDescription).Value)
-
-		time, _ := val.GetStartAt()
-		fmt.Println(time.Local())
+func timeOrNull(time time.Time, err error) *time.Time {
+	if err == nil {
+		return &time
+	} else {
+		return nil
 	}
 }
